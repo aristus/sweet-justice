15623ddc127a698b7dbf2c422871ff04eea24c06
Carlos Bueno <carlos@bueno.org>
2010-04-22 20:07:50 -0700

    half-fix for copy/paste

--- sweet-justice.js
+++ sweet-justice.js
@@ -21,12 +21,13 @@
 !function() {
 
   // don't break up short words.
-  var MIN_WORD = 5;
+  var MIN_WORD = 6;
 
   // don't mess with the content of these tags.
   var tag_blacklist = {
     'code': true,
-    'pre': true
+    'pre': true,
+    'abbr': true
   }
 
   // Recurse raw DOM nodes, transforming each text node.
@@ -48,7 +49,7 @@
     }
   }
 
-  // Given a plain-text string, insert shy hyphens into long words.
+  // Given a plain-text string, insert shy-phens into long words.
   // Does *not* follow English syllable rules, but tries to be som-
   // ewhat sensible. Whitespace is not preserved.
   function break_dance(text) {
@@ -56,18 +57,63 @@
     for (var i=0; i<words.length; i++) {
       var word = words[i];
       if (word.length >= MIN_WORD && word.charAt(0) !== '&') {
-        words[i] = word.replace(simple, insert).replace(/\u00AD$/, '');
+        words[i] = word.replace(simple, insert)
+          .replace(/\u00AD(.{0,1})$/, '$1'); // no "hyphen-s"
       }
     }
     return words.join(' ');
   }
 
   var faux_english = /.{1,3}[aeiouy]/gi;
-  var simple = /.{1,3}[^\-]/g;
+  var simple = /[\w]{1,4}/g;
 
   function insert(text) {
     //return text + '*'; // debugging
     return text + '\u00AD';
+  }
+
+  // the shy-phen character is an odd duck. on copy/paste
+  // most systems will translate them to hard spaces, which is
+  // usually not what you want. so on copy we take 'em out.
+  // The selection APIs are very different across browsers
+  // so there is a lot of jazzhands in this function.
+  function copy_protect(e) {
+    var body = document.getElementsByTagName("body")[0];
+    var sel = '';
+    if (typeof window.getSelection != "undefined") {
+      sel = window.getSelection();
+      var range = sel.getRangeAt(0);
+      var shadow = document.createElement("div");
+      shadow.style.overflow = 'hidden';
+      shadow.style.position = 'absolute';
+      shadow.style.top = '-5000px';
+      shadow.style.height = '1px';
+      body.appendChild(shadow);
+      shadow.appendChild(range.cloneContents());
+      shadow.innerHTML = shadow.innerHTML.replace(/\u00AD/g, '');
+      sel.selectAllChildren(shadow);
+      window.setTimeout(function() {
+        shadow.parentNode.removeChild(shadow);
+        if (typeof window.getSelection().setBaseAndExtent != "undefined") {
+          sel.setBaseAndExtent(
+            range.startContainer,
+            range.startOffset,
+            range.endContainer,
+            range.endOffset
+          );
+        }
+      },0);
+    } else {
+      // Internet Explorer. need halp! :(
+      // sel = document.selection;
+      // var range = sel.createRange();
+      // var shadow = document.createElement("div");
+      // shadow.innerHTML = range.htmlText.replace(/\u00AD/g, '');
+      // var range2 = body.createTextRange();
+      // range2.moveToElementText(shadow);
+      // range2.select();
+    }
+    return;
   }
 
   // jQuery
@@ -76,6 +122,7 @@
       $(el).css({'text-align':'justify'});
       justify_my_love(el);
     });
+    $('body').bind('copy', copy_protect);
   }
 
   // YUI3
@@ -84,6 +131,14 @@
       el.setStyle('text-align','justify');
       justify_my_love(el._node);
     });
+
+    // Y.one('body').on('copy', copy_protect); //hmm.YUI3 doesn't work with this.
+    var body = document.getElementsByTagName("body")[0];
+    if (window.addEventListener) {
+      body.addEventListener("copy", copy_protect, false);
+    } else {
+      body.attachEvent("oncopy", copy_protect);
+    }
   }
 
   // dispatch on library
