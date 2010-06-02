2a0f9f9134bdeb5e0fb5936d17e02a3b199380f3
Carlos Bueno <carlos@bueno.org>
2010-06-01 19:59:40 -0700

    for great justice

--- sweet-justice.js
+++ sweet-justice.js
@@ -108,7 +108,7 @@
   // TeX algo: split on well-known prefixes and suffixes
   // then along the vccv line. This is not i18n nor even
   // generally correct, but is fairly compact.
-  var presuf = /^(\W*)(anti|auto|ab|an|ax|al|as|bi|be|contra|cat|cath|cir|cum|cog|col|com|con|cor|could|co|desk|de|dis|did|dif|di|eas|every|ever|extra|ex|en|em|epi|evi|fin|hyst|hy|han|il|in|im|ir|just|jus|lig|lit|li|mech|manu|man|mal|mis|mid|mono|multi|mem|micro|non|nano|ob|oc|of|op|over|para|per|post|pre|peo|pro|retro|rea|re|rhy|should|some|semi|sen|sol|sub|suc|suf|super|sup|sur|sus|syn|sym|syl|tech|trans|tri|typo|type|uni|un|would|won)?(.*?)(icals?|ables?|ings?|tions?|ions?|ies|isms?|ists?|ful|ness|ments?|ly|ify|ize|ise|ity|en|ers?|ences?|tures?|ples?|als?|phy|phies|ry|ries|cy|cies|mums?|ous|cents?)?(\W*)$/i;
+  var presuf = /^(\W*)(anti|auto|ab|an|ax|al|as|bi|bet|be|contra|cat|cath|cir|cum|cog|col|com|con|cor|could|co|desk|de|dis|did|dif|di|eas|every|ever|extra|ex|end|en|em|epi|evi|func|fund|fin|hyst|hy|han|il|in|im|ir|just|jus|loc|lig|lit|li|mech|manu|man|mal|mis|mid|mono|multi|mem|micro|non|nano|ob|oc|of|opt|op|over|para|per|post|pre|peo|pro|retro|rea|re|rhy|should|some|semi|sen|sol|sub|suc|suf|super|sup|sur|sus|syn|sym|syl|tech|trans|tri|typo|type|uni|un|van|vert|with|would|won)?(.*?)(weens?|widths?|icals?|ables?|ings?|tions?|ions?|ies|isms?|ists?|ful|ness|ments?|ly|ify|ize|ise|ity|en|ers?|ences?|tures?|ples?|als?|phy|puts?|phies|ry|ries|cy|cies|mums?|ous|cents?)?(\W*)$/i;
 
   function break_word_en(word) {
     // punctuation, prefix, center, suffix, punctuation
